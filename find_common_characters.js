f824c2f3cd96f7fe4d8a3aa485fbc3e66596ce91
Tushar  Borole <tushariscoolster@gmail.com>
2019-05-18 21:55:44 -0700

    Find common character in array of string

---
+++ find_common_characters.js
@@ -0,0 +1,49 @@
+// 1002. Find Common Characters
+
+// Given an array A of strings made only from lowercase letters, return a list of all characters that show up in all strings within the list (including duplicates).  For example, if a character occurs 3 times in all strings but not 4 times, you need to include that character three times in the final answer.
+//
+//     You may return the answer in any order.
+//
+//
+//
+//     Example 1:
+//
+// Input: ["bella","label","roller"]
+// Output: ["e","l","l"]
+// Example 2:
+//
+// Input: ["cool","lock","cook"]
+// Output: ["c","o"]
+//
+//
+// Note:
+//
+//     1 <= A.length <= 100
+// 1 <= A[i].length <= 100
+// A[i][j] is a lowercase letter
+
+/**
+ * @param {string[]} A
+ * @return {string[]}
+ */
+var commonChars = function(A) {
+  let formattedArray = A.map(val => val.split("")); //?
+  let start = formattedArray[0];
+  const commonCharacter = [];
+  for (let val of start) {
+    const foundArray = [];
+    for (let i = 1; i < formattedArray.length; i++) {
+      const index = formattedArray[i].indexOf(val); //?
+      const foundStatus = index > -1;
+      foundArray.push(foundStatus);
+      if (foundStatus) {
+        formattedArray[i][index] = "found";
+      }
+    }
+    if (!foundArray.includes(false)) {
+      commonCharacter.push(val);
+    }
+  }
+
+  return commonCharacter;
+};
---
+++ move_zeroes.js
@@ -0,0 +1,34 @@
+// Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+//
+// Example:
+//
+//     Input: [0,1,0,3,12]
+// Output: [1,3,12,0,0]
+// Note:
+//
+//     You must do this in-place without making a copy of the array.
+//     Minimize the total number of operations.
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+const moveZeroes = function(nums) {
+  let y = 0; // y is none zero pointer
+
+  // y only increase when i found a none zero number
+  // i only swap if i found a none zero number
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      const tmp = nums[i];
+      nums[i] = nums[y];
+      nums[y] = tmp;
+      y++;
+      console.log(y);
+    }
+  }
+  nums;
+};
+
+const array = [0, 1, 0, 3, 12];
+console.log(moveZeroes(array));
