375eefeb53487d89b0a26ca9e518a38fa5ecf396
Tushar  Borole <tushariscoolster@gmail.com>
2019-07-08 20:39:29 -0700

    340. Longest Substring with At Most K Distinct Characters

--- k_distinct_characters.js
+++ k_distinct_characters.js
@@ -1,3 +1,19 @@
+// 340. Longest Substring with At Most K Distinct Characters
+//
+//
+// Given a string, find the length of the longest substring T that contains at most k distinct characters.
+//
+//     Example 1:
+//
+// Input: s = "eceba", k = 2
+// Output: 3
+// Explanation: T is "ece" which its length is 3.
+// Example 2:
+//
+// Input: s = "aa", k = 1
+// Output: 2
+// Explanation: T is "aa" which its length is 2.
+
 /**
  * @param {string} s
  * @param {number} k
