2ea4768c5222e2ac7f30ca1662c140fa741a422a
Tushar  Borole <tushariscoolster@gmail.com>
2019-07-22 20:26:46 -0700

    70. Climbing Stairs

---
+++ climbing_stairs.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n, memo = []) {
+  /*
+    0 distinct ways to climb negative steps if we
+    can only take 1 or 2 steps
+  */
+  if (n === 0) {
+    return 1;
+  }
+
+  /*
+     1 distinct way to climb 1 if we can only take 1
+     or 2 steps.
+     We take 1 step.
+      */
+  if (n < 0) {
+    return 0;
+  }
+
+  if (memo[n] > 0) {
+    return memo[n];
+  }
+
+  const left = climbStairs(n - 1, memo);
+  const right = climbStairs(n - 2, memo);
+  memo[n] = left + right;
+  return memo[n];
+};
+
+climbStairs(38);
